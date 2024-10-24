6c91b59a69e7c13001c2adf07199eb1438a12f00
LeonardoMMFontoura <leonardommfontoura@gmail.com>
2024-10-23 23:14:54 -0300

    restarting 100 days repo

--- Day-1-Band_Name_Generator.py
+++
@@ -1,8 +0,0 @@
-#Salute
-print("Welcome to the Band Name Generator")
-#Ask user for a city
-city = input("Please tell me the name of a city you like\n") 
-#Ask user for a pet name
-pet_name = input("Please tell me the name of a pet name you had or have\n")
-#Combine the name of band and pet
-print(f" Your band's name is {city} {pet_name}")
