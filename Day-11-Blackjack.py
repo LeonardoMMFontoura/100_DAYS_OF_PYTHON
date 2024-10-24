6f0432f28711b219594e6b275a3246f2a381b34c
LeonardoMMFontoura <leonardommfontoura@gmail.com>
2024-10-23 23:42:58 -0300

    Day 11. Blackjack initial setup

--- Day-11-Blackjack.py
+++ Day-11-Blackjack.py
@@ -1,16 +1,17 @@
 logo = """
-.------.            _     _            _    _            _    
-|A_  _ |.          | |   | |          | |  (_)          | |   
-|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
-| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
-|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
-`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
-      |  \/ K|                            _/ |                
-      `------'                           |__/           
+.-- ----._ _ _ _ _
+| A_ _ |.| |||||(_) | |
+|(\/).-- ---.| |__ | |__ _ ___ | |___ __ _ ___ | |__
+| \/|K / \||'_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+| \/|/\|||_) | |(_ | |(__ | <||(_ | |(__ | <
+  `-----|\/||_.__ / |_ | \__, _ | \___ | _ | \_ \ |\__, _ | \___ | _ | \_ \\
+				      |\/K | _ / |
+			  `------'                           |__/           '
+''
 """
-                   
 
-                                      
+
+
 ############### Blackjack Project #####################
 
 #Difficulty Normal 😎: Use all Hints below to complete the project.
@@ -20,8 +21,8 @@ logo = """
 
 ############### Our Blackjack House Rules #####################
 
-## The deck is unlimited in size. 
-## There are no jokers. 
+## The deck is unlimited in size.
+## There are no jokers.
 ## The Jack/Queen/King all count as 10.
 ## The the Ace can count as 11 or 1.
 ## Use the following list as the deck of cards:
@@ -31,17 +32,16 @@ logo = """
 
 ##################### Hints #####################
 
-#Hint 1: Go to this website and try out the Blackjack game: 
-#   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here: 
-#   http://blackjack-final.appbrewery.repl.run
+#Hint 1: Go to this website and try out the Blackjack game:
+#https://games.washingtonpost.com/games/blackjack/
+#Then try out the completed Blackjack project here:
+#http://blackjack-final.appbrewery.repl.run
 
-#Hint 2: Read this breakdown of program requirements: 
-#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
+#Hint 2: Read this breakdown of program requirements:
+#http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
 #Then try to create your own flowchart for the program.
 
-#Hint 3: Download and read this flow chart I've created: 
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
+#Hint 3: Download and read this flow chart I've created:
+#https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
 
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
-#11 is the Ace.
